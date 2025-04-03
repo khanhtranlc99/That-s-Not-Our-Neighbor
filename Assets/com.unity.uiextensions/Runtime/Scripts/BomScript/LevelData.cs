3bfb56c84b265ff790c16bbacba5d72fbcfd0b17
DESKTOP-SATPE72\T plus Computer <trankhanhlc99@gmail.com>
2025-04-04 00:25:04 +0700

    Base

---
+++ Assets/com.unity.uiextensions/Runtime/Scripts/BomScript/LevelData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using System;
+using DG.Tweening;
+public enum Difficult
+{
+    Normal,
+    Hard,
+    VeryHard,
+    Boss
+
+}
+
+public class LevelData : SerializedMonoBehaviour
+{
+ 
+    public void Init()
+    {
+    
+      
+   
+    }
+ }
